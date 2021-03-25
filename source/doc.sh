ff55af7567c291339e9bac0bf10ac9374306f95f
Andrij Felenko <andrij.felenko@tuta.io>
2021-03-25 17:35:12 +0200

    add script and doc creator

--- source/doc.sh
+++ source/doc.sh
@@ -1,7 +1,22 @@
 cd base_af
 doxygen doxygen_script
+ln -s -f doc/html/index.html doc/af_lib.html
 
-cd ../..
-mkdir doc
+cd ../projects/battle_for_rokugan
+doxygen doxygen_script
+ln -s -f doc/html/index.html doc/bfr.html
+
+cd ../latinka_translitterator
+doxygen doxygen_script
+ln -s -f doc/html/index.html doc/latinka.html
+
+cd ../../..
+if [ ! -d "doc" ]
+then 
+    mkdir doc
+fi
 cd doc
-ln -s ../source/base_af/doc/html/index.html af_lib.html
+
+ln -s -f ../source/base_af/doc/html/index.html af_lib.html
+ln -s -f ../source/projects/battle_for_rokugan/doc/html/index.html bfr.html
+ln -s -f ../source/projects/latinka_translitterator/doc/html/index.html latinka.html
