986c66fdfbfd5b15b0edd071e4bf375bee4f57d1
andrij-felenko <andrij.felenko@tuts.io>
2021-04-07 18:09:23 +0300

    update doc

--- source/doc.sh
+++ source/doc.sh
@@ -18,7 +18,7 @@ doc_dir=$(pwd)
 # 1 - path to folder
 # 2 - name of html file
 function make_doc {
-    cd ${src_dir}$1
+    cd "${src_dir}/$1"
     git pull
     doxygen doxygen_script
     
@@ -27,22 +27,21 @@ function make_doc {
     ln -s -f html/index.html "${doc_dir}/$2.html"
     git add *
     git commit -m "update doc $now"
-    git config credential.helper store
-    git push
+    
+    cd ${src_dir}
+    git add $1
     
     cd ${doc_dir}
-    ln -s -f "${src_dir}$1/doc/html/index.html" "$2.html"
+    ln -s -f "${src_dir}/$1/doc/html/index.html" "$2.html"
+    git add "$2.html"
 }
 
-make_doc "/base_af" "af_lib"
-make_doc "/projects/battle_for_rokugan" "bfr"
-make_doc "/projects/latinka_translitterator" "latinka"
+make_doc "base_af" "af_lib"
+make_doc "projects/battle_for_rokugan" "bfr"
+make_doc "projects/latinka_translitterator" "latinka"
 
 cd ${src_dir}
 git pull
-git add base_af
-git add projects/battle_for_rokugan
-git add projects/latinka_translitterator
-git add ${doc_dir}"/*"
 git commit -m "update doc $now"
-git push
+git push --recurse-submodules=on-demand
+
