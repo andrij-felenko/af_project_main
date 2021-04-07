0c8972e5b1a78de8fc9314e8280a7db5f39f2752
andrij-felenko <andrij.felenko@tuts.io>
2021-04-07 17:40:11 +0300

    update doc

--- source/doc.sh
+++ source/doc.sh
@@ -1,51 +1,45 @@
-cd base_af
-git pull
-doxygen doxygen_script
-cd doc
-ln -s -f html/index.html af_lib.html
-cd ..
-git add doc/*
-git add af_lib.html
-git commit -m "update doc"
-git push
+src_dir=$(pwd)
 
-cd ../projects/battle_for_rokugan
-git pull
-doxygen doxygen_script
-cd doc
-ln -s -f html/index.html bfr.html
-cd ..
-git add doc/*
-git add bfr.html
-git commit -m "update doc"
-git push
+function mkdir_ {
+    if [ ! -d $1 ] 
+        then mkdir $1
+    fi
+}
 
-cd ../latinka_translitterator
-git pull
-doxygen doxygen_script
-cd doc
-ln -s -f html/index.html latinka.html
-cd ..
-git add doc/*
-git add latinka.html
-git commit -m "update doc"
-git push
+function mkdir_and_open {
+    mkdir_ $1
+    cd $1
+}
 
-cd ../..
+cd ..
+mkdir_and_open "doc"
+doc_dir=$(pwd)
+
+# 1 - path to folder
+# 2 - name of html file
+function make_doc {
+    cd ${src_dir}$1
+    git pull
+    doxygen doxygen_script
+    cd doc
+    ln -s -f html/index.html $2".html"
+    cd ${src_dir}$1
+    git add doc/*
+    git add af_lib.html
+    git commit -m "update doc $now"
+    ln -s -f html/index.html ${doc_dir}$2".html"
+    git push
+}
+
+make_doc "base_af" "af_lib"
+make_doc "projects/battle_for_rokugan" "bfr"
+make_doc "projects/latinka_translitterator" "latinka"
+
+cd ${src_dir}
 git pull
 git add base_af
 git add projects/battle_for_rokugan
 git add projects/latinka_translitterator
-git commit -m "update doc"
+git add ../doc/*
+git commit -m "update doc"$now
 git push
-
-cd ..
-if [ ! -d "doc" ]
-then 
-    mkdir doc
-fi
-cd doc
-
-ln -s -f ../source/base_af/doc/html/index.html af_lib.html
-ln -s -f ../source/projects/battle_for_rokugan/doc/html/index.html bfr.html
-ln -s -f ../source/projects/latinka_translitterator/doc/html/index.html latinka.html
